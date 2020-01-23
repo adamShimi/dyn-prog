b0d53f3f1ced19ae82cbb3fb96e5ab19b395b956
Adam Shimi <adam.shimi@toulouse-inp.fr>
2020-01-23 23:49:48 +0100

    Bootstrap API.

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+pub fn find_optimal(_prob : MDP) -> Policy {
+  unimplemented!("Still everything to do!");
+}
+
+pub struct MDP;
+pub struct Policy;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
