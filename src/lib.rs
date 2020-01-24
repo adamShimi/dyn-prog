e113b31f90cc3d2d5aa1705e6f7097aa9a313449
Adam Shimi <adam.shimi@toulouse-inp.fr>
2020-01-24 15:31:31 +0100

    Expand MDP and Policy, and add basic types.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,24 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+
 pub fn find_optimal(_prob : MDP) -> Policy {
   unimplemented!("Still everything to do!");
 }
 
-pub struct MDP;
-pub struct Policy;
+pub struct State;
+pub struct Action;
+
+// Deterministic policy
+type Policy = HashMap<State,Action>;
+
+pub struct MDP {
+  states : HashSet<State>,
+  actions : HashSet<Action>,
+  // Link a 4-tuple (state,action,reward,next state) with
+  // the probability to get reward and next state when
+  // using action in state.
+  dynamics : HashSet<(State,Action,f64,State),f64>,
+}
 
 #[cfg(test)]
 mod tests {
