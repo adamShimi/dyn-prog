efa4ff9ce05326d6ccb461f2c3858ac1c81e99a5
Adam Shimi <adam.shimi@toulouse-inp.fr>
2020-01-25 16:03:41 +0100

    Clean code for deterministic MDP (for starters).

--- src/lib.rs
+++ src/lib.rs
@@ -2,36 +2,29 @@ use std::collections::HashMap;
 
 use std::hash::Hash;
 
-pub fn find_optimal<'a,S,A,M>(_prob : &M) -> Policy<A>
+pub fn find_optimal<'a,S,A>(_prob : &MDP<'a,S,A>) -> Policy
   where S : State,
-        A : Action,
-        M : MDP<'a,S,A> {
+        A : Action {
   unimplemented!("Still everything to do!");
 }
 
-fn policy_evaluation<'a,S,A,M>(prob : &M,
-                               pol : &Policy<A>,
+fn policy_evaluation<'a,S,A,M>(prob : &MDP<'a,S,A>,
+                               pol : &Policy,
                                thresh : f64) -> StateValue
   where S : State,
-        A : Action,
-        M : MDP<'a,S,A> {
+        A : Action {
 
   let mut max_diff = thresh;
-  let mut value = vec![0.0; prob.states().len()];
+  let mut value = vec![0.0; prob.states.len()];
 
   while max_diff >= thresh - std::f64::EPSILON {
-    for (index,state) in prob.states().iter().enumerate() {
-      let update : f64 =
-        prob.rewards().iter()
-                    .zip(prob.states().iter().enumerate())
-                    .map(|(reward,(index_next,next_state))|
-                      prob.dynamics(state,
-                                    &pol.choice[index],
-                                    *reward,
-                                    next_state)
-                      *((*reward as f64)+prob.discount()*value[index_next])
-                    )
-                    .sum();
+    for index in 0..prob.states.len() {
+      let (reward,index_next) = prob.dynamics.get(&(index,
+                                                    *pol.choice.get(index).unwrap()
+                                                   )
+                                             )
+                                             .unwrap();
+      let update : f64 = (*reward as f64)+prob.discount*value[*index_next];
       max_diff = max_diff.max((update-value[index]).abs());
       value[index] = update;
     }
@@ -39,67 +32,37 @@ fn policy_evaluation<'a,S,A,M>(prob : &M,
   StateValue {value}
 }
 
-fn policy_improvement<'a,S,A,M>(prob : &M,
-                                pol : &Policy<A>,
-                                val : &StateValue) -> Policy<A>
+fn policy_improvement<'a,S,A,M>(_prob : &MDP<'a,S,A>,
+                                _pol : &Policy,
+                                _val : &StateValue) -> Policy
   where S : State,
-        A : Action,
-        M : MDP<'a,S,A> {
+        A : Action {
 
   unimplemented!("Second block of General Policy Iteration");
 }
 
-pub trait State : Eq + Hash + Copy + 'static {}
-pub trait Action : Eq + Hash + Copy + 'static {}
+pub trait State : Eq + Hash + Copy {}
+pub trait Action : Eq + Hash + Copy {}
 
-// Deterministic policy
-pub struct Policy<A : Action> {
-  choice : Vec<A>,
+// Deterministic policy that gives the index of the chosen action
+// for the indexed state.
+pub struct Policy {
+  choice : Vec<usize>,
 }
 
 pub struct StateValue {
   value : Vec<f64>,
 }
 
-pub trait MDP<'a,S,A> {
-  fn states(&self) -> &'a [S];
-  fn actions(&self) -> &'a [A];
-  fn rewards(&self) -> &'a [isize];
-  fn discount(&self) -> f64;
-  fn dynamics(&self, state : &S, action : &A, reward : isize, next_state : &S) -> f64;
-}
-
-pub struct TabMDP<'a, S : State, A : Action> {
+// Deterministic MDP
+pub struct MDP<'a, S : State, A : Action> {
   pub states : &'a [S],
   pub actions : &'a [A],
   pub rewards : &'a [isize],
   pub discount : f64,
-  // Link a 4-tuple (state,action,reward,next state) with
-  // the probability to get reward and next state when
-  // using action in state.
-  pub dynamics : HashMap<(S,A,isize,S),usize>,
-}
-
-impl<'a,S,A> MDP<'a,S,A> for TabMDP<'a,S,A>
-  where S : State,
-        A : Action {
-
-  fn states(&self) -> &'a [S] {
-    self.states
-  }
-  fn actions(&self) -> &'a [A] {
-    self.actions
-  }
-  fn rewards(&self) -> &'a [isize] {
-    self.rewards
-  }
-  fn discount(&self) -> f64 {
-    self.discount
-  }
-  fn dynamics(&self, state : &S, action : &A, reward : isize, next_state : &S) -> f64 {
-    (*self.dynamics.get(&(*state,*action,reward,*next_state)).unwrap() as f64)/
-      (self.actions.len() as f64)
-  }
+  // Link a tuple (index state,index action) deterministically
+  // to the tuple (reward, index next state).
+  pub dynamics : HashMap<(usize,usize),(isize,usize)>,
 }
 
 #[cfg(test)]
