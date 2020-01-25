15f7e8332b2e560c5bc85f5c887ad1a64378daa7
Adam Shimi <adam.shimi@toulouse-inp.fr>
2020-01-25 14:04:22 +0100

    Clean types.

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,23 @@
-use std::collections::HashMap;
-use std::collections::HashSet;
+use std::collections::BTreeMap;
 
-pub fn find_optimal<S,A>(_prob : &MDP<S,A>) -> Policy<S,A>
+pub fn find_optimal<'a,S,A>(_prob : &MDP<'a,S,A>) -> Policy<'a,A>
   where S : State,
         A : Action {
   unimplemented!("Still everything to do!");
 }
 
-fn policy_evaluation<S,A>(_prob : &MDP<S,A>,
-                          _pol : &Policy<S,A>,
-                          _thresh : f64) -> StateValue<S>
+fn policy_evaluation<'a,S,A>(_prob : &MDP<'a,S,A>,
+                             _pol : &Policy<'a,A>,
+                             _thresh : f64) -> StateValue<'a>
   where S : State,
         A : Action {
 
   unimplemented!("First block of General Policy Iteration");
 }
 
-fn policy_improvement<S,A>(_prob : &MDP<S,A>,
-                           _pol : &Policy<S,A>,
-                           _val : &StateValue<S>) -> Policy<S,A>
+fn policy_improvement<'a,S,A>(_prob : &MDP<'a,S,A>,
+                              _pol : &Policy<'a,A>,
+                              _val : &StateValue<'a>) -> Policy<'a,A>
   where S : State,
         A : Action {
 
@@ -29,21 +28,23 @@ pub trait State {}
 pub trait Action {}
 
 // Deterministic policy
-pub struct Policy<S : State, A : Action> {
-  choice : HashMap<S,A>,
+pub struct Policy<'a, A : Action> {
+  choice : &'a [A],
 }
 
-pub struct StateValue<S : State> {
-  value : HashMap<S,f64>,
+pub struct StateValue<'a> {
+  value : &'a [f64],
 }
 
-pub struct MDP<S : State, A : Action> {
-  states : HashSet<S>,
-  actions : HashSet<A>,
+pub struct MDP<'a, S : State, A : Action> {
+  pub states : &'a [S],
+  pub actions : &'a [A],
+  pub rewards : &'a [isize],
+  pub discount : f64,
   // Link a 4-tuple (state,action,reward,next state) with
   // the probability to get reward and next state when
   // using action in state.
-  dynamics : HashSet<(S,A,f64,S),f64>,
+  pub dynamics : BTreeMap<(S,A,isize,S),f64>,
 }
 
 #[cfg(test)]
