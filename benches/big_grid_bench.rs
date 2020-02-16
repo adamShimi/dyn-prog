bbfe677d2465a8cf5fd08d6ee1e0e3ef964cd183
Adam Shimi <adam.shimi@toulouse-inp.fr>
2020-02-16 20:07:21 +0100

    Add bench for value iteration.

--- benches/big_grid_bench.rs
+++ benches/big_grid_bench.rs
@@ -5,8 +5,12 @@ use dyn_prog::mdp::grid_world::{GridWorld,GridState};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let mdp = GridWorld::new(10,10,GridState{row:0,col:0},GridState{row:8,col:4},0.9);
-    let gpi = GPIVersion::PolicyIteration {thresh : std::f64::EPSILON};
-    c.bench_function("grid 10x10", |b| b.iter(|| find_optimal(black_box(&mdp),black_box(gpi))));
+    let gpi_pol = GPIVersion::PolicyIteration {thresh : std::f64::EPSILON};
+    let gpi_val = GPIVersion::ValueIteration;
+    c.bench_function("grid polIter 10x10",
+                     |b| b.iter(|| find_optimal(black_box(&mdp),black_box(gpi_pol))));
+    c.bench_function("grid valIter 10x10",
+                     |b| b.iter(|| find_optimal(black_box(&mdp),black_box(gpi_val))));
 }
 
 criterion_group!(benches, criterion_benchmark);
